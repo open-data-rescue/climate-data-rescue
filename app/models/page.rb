255a038a586729ae836b906d5a69acde0a2a79fd
Tim <timbrasche@gmail.com>
2016-02-25 00:25:57 -0500

    transcription app fixes

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,19 +10,28 @@ class Page < ActiveRecord::Base
                   :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :upload, :content_type => /\Aimage\/.*\Z/
   after_create :set_name_to_filename
+  before_save :extract_upload_dimensions
+  after_create :parse_filename
   
   # after_create :parse_filename
   def parse_filename
     if self.upload.present?
       filename = self.upload_file_name
       components = filename.split("_")
-      self.accession_number = String.parse(components[1])
-      self.ledger_id = String.parse(components[2])
-      self.ledger_volume = String.parse(components[3])
+      unless components.count == 6
+        raise "invalid filename"
+      end
+      self.accession_number = components[1]
+      if components[2].count == 1
+        self.ledger_id = components[2]
+      else  
+        raise "invalid filename"
+      end
+      self.ledger_volume = components[3]
       self.from_date = Date.parse(components[4])
       self.to_date = Date.parse(components[5])
-      self.pagetype_id = String.parse(components[6])
-      self.save
+      self.pagetype_id = components[6]
+      self.save!
     end
   end
   
@@ -61,7 +70,7 @@ class Page < ActiveRecord::Base
       geometry = Paperclip::Geometry.from_file(tempfile)
       self.width = geometry.width.to_i
       self.height = geometry.height.to_i
-      self.save
+      # self.save
     end
   end
   #sets a scope for all transcribable pages to be those that are not done
@@ -77,8 +86,12 @@ class Page < ActiveRecord::Base
 
   # Don't want the image to be squashed
   def display_height
-    disp_height = (1.0 * self.display_width / self.width) * self.height
-    return disp_height.to_i
+    if self.display_width
+      disp_height = (1.0 * self.display_width / self.width) * self.height
+      disp_height.to_i
+    else
+      self.height
+    end
   end
   #on new transcription creation, increment the classification count of its associated page
   def increment_classification_count
