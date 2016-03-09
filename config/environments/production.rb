dc3aa57148610b87afe57efe77747a7b72ee6e82
Robert <robert@grenadine.co>
2016-03-08 23:30:21 -0500

    set log level to debug in prd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Weather::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
+  config.log_level = :debug
+  
   config.eager_load = true
   
   # Code is not reloaded between requests
