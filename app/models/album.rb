653a0dadf3a737d8ad76bf55110e533b6b7ddc18
Robert <robert@grenadine.co>
2016-03-11 23:12:50 -0500

    deleted albums and photos

--- app/models/album.rb
+++
@@ -1,4 +0,0 @@
-class Album < ActiveRecord::Base
-  # attr_accessible :name
-  has_many :photos
-end
--- app/models/photo.rb
+++
@@ -1,7 +0,0 @@
-class Photo < ActiveRecord::Base
-  #attr_accessible :album_id, :name, :upload
-  belongs_to :album
-
-  has_attached_file :upload, :styles => { :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :upload, :content_type => /\Aimage\/.*\Z/
-end
