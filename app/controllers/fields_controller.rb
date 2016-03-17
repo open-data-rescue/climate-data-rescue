4237cb7e41678df289bd89f8cac42e80b4c98006
Robert <robert@grenadine.co>
2016-03-17 10:10:03 -0400

    can create fields now, and edit

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -167,6 +167,6 @@ class FieldsController < ApplicationController
   
   private
   def field_params
-    params.require(:field).permit(:field_key, :initial_value, :kind, :name, :options, :validations)
+    params.require(:field).permit(:field_key, :initial_value, :kind, :name, :options, :validations, :field_group_id)
   end
 end
