778d9f6346e7d1f1b649455e10b0af32dc0acdb3
Robert <robert@grenadine.co>
2016-03-28 22:22:14 -0400

    wrap in app role

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,10 +75,12 @@ namespace :deploy do
 
   desc "Symlink shared config files"
   task :symlink_config_files do
+    on roles(:app) do
       execute "ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
       execute "ln -s #{ deploy_to }/shared/config/initializers/secret_token.rb #{ current_path }/config/initializers/secret_token.rb"
       execute "ln -s #{ deploy_to }/shared/log #{ current_path }/log"
       execute "ln -s #{ deploy_to }/shared/tmp #{ current_path }/tmp"
+    end
   end
 
   before :starting,     :check_revision
