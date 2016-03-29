7228c7a56b2c2cf8e2f75295e6bdb34e6d52bda6
Robert <robert@grenadine.co>
2016-03-28 21:15:56 -0400

    separated out deploy code into their environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-set :branch, ENV['BRANCH'] || "production"
+
 
 server '132.216.28.226', port: 22, roles: [:web, :app, :db], primary: true
 
@@ -12,9 +12,7 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/opt/www/ClimateDataRescue"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
+set :branch, ENV['BRANCH'] || "production"
+set :stage,           :production
+
+set :deploy_to,       "/opt/www/ClimateDataRescue"
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
