f0a205f26a3ba722b80eea17b52dc617a0117b83
Robert <robert@grenadine.co>
2016-04-03 17:19:46 -0400

    updated reqs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331213952) do
+ActiveRecord::Schema.define(version: 20160403202102) do
 
   create_table "annotations", force: :cascade do |t|
     t.integer  "x_tl",             limit: 4
