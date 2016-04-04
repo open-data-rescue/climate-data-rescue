e5d71976ec30c69564c483f024db059406f6a360
Robert <robert@grenadine.co>
2016-04-03 22:27:36 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403202102) do
+ActiveRecord::Schema.define(version: 20160404020411) do
 
   create_table "annotations", force: :cascade do |t|
     t.integer  "x_tl",             limit: 4
@@ -23,11 +23,12 @@ ActiveRecord::Schema.define(version: 20160403202102) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "date_time_id",     limit: 255
+    t.integer  "field_group_id",   limit: 4
   end
 
   create_table "data_entries", force: :cascade do |t|
     t.string  "value",         limit: 255
-    t.string  "type",          limit: 255
+    t.string  "data_type",     limit: 255
     t.integer "user_id",       limit: 4
     t.integer "page_id",       limit: 4
     t.integer "annotation_id", limit: 4
@@ -44,14 +45,14 @@ ActiveRecord::Schema.define(version: 20160403202102) do
   end
 
   create_table "fields", force: :cascade do |t|
-    t.string   "name",           limit: 255
-    t.string   "field_key",      limit: 255
-    t.string   "kind",           limit: 255
-    t.string   "initial_value",  limit: 255
-    t.text     "options",        limit: 65535
-    t.text     "validations",    limit: 65535
-    t.integer  "field_group_id", limit: 4
-    t.string   "data_type",      limit: 255
+    t.string   "name",            limit: 255
+    t.string   "field_key",       limit: 255
+    t.string   "html_field_type", limit: 255
+    t.string   "initial_value",   limit: 255
+    t.text     "options",         limit: 65535
+    t.text     "validations",     limit: 65535
+    t.integer  "field_group_id",  limit: 4
+    t.string   "data_type",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -82,7 +83,7 @@ ActiveRecord::Schema.define(version: 20160403202102) do
     t.integer  "width",                limit: 4
     t.integer  "order",                limit: 4
     t.integer  "page_type_id",         limit: 4
-    t.boolean  "done",                 limit: 1,   default: false, null: false
+    t.boolean  "done",                             default: false, null: false
     t.integer  "classification_count", limit: 4,   default: 0,     null: false
     t.string   "accession_number",     limit: 255
     t.date     "start_date"
@@ -109,7 +110,7 @@ ActiveRecord::Schema.define(version: 20160403202102) do
     t.string   "key",            limit: 255
     t.text     "value",          limit: 65535
     t.text     "interpolations", limit: 65535
-    t.boolean  "is_proc",        limit: 1,     default: false
+    t.boolean  "is_proc",                      default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -129,7 +130,7 @@ ActiveRecord::Schema.define(version: 20160403202102) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "display_name",           limit: 255
-    t.boolean  "admin",                  limit: 1
+    t.boolean  "admin"
     t.text     "bio",                    limit: 65535
     t.integer  "num_contributions",      limit: 4,     default: 0,  null: false
     t.datetime "created_at"
