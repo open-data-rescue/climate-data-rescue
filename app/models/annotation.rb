da08dc9bc168b32b8b2e9b15b5fb754a37e85955
Robert <robert@grenadine.co>
2016-04-07 21:07:30 -0400

    update relationship to data entries

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,7 +1,7 @@
 class Annotation < ActiveRecord::Base
   #attr_accessible :bounds, :data, :transcription_id, :fieldgroup_id, :page_id
   belongs_to :transcription
-  has_and_belongs_to_many :fields, through: :annotations_data
+  has_many :data_entries
   
   #TODO: make another data type to hold the join and the data, AnnotationData or something
 end
