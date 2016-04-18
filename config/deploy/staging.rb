b59fc9545b424f8a0cf18ae4f0aeaa3206f53292
Robert <robert@grenadine.co>
2016-04-18 18:23:31 -0400

    set puma config path for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,8 @@ set :deploy_to,       "/opt/www/climate_test"
 
 set :application,     'climate_test'
 
-set :puma_bind,       %w(tcp://0.0.0.0:3030 "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock")
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_conf,       "#{shared_path}/puma.rb"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
