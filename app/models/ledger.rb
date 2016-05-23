8923ce5c87048279cc65698d5057638b275635c6
Robert <robert@grenadine.co>
2016-05-23 19:32:39 -0400

    added relationship from ledger to pages

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -2,4 +2,5 @@ class Ledger < ActiveRecord::Base
   #attr_accessible :author, :extern_ref, :title, :pagetype_id
   #TODO: remove :pagetype_id attribute from database and attr_accessible and update the form views for ledger
   has_many :page_types
+  has_many :pages, through: :page_types
 end
