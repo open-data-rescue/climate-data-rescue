804431a5f3228862b56940a638b63c1c59400ae3
Robert <robert@grenadine.co>
2016-05-24 22:36:16 -0400

    set dependent destroys

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,6 +1,6 @@
 class Ledger < ActiveRecord::Base
   #attr_accessible :author, :extern_ref, :title, :pagetype_id
   #TODO: remove :pagetype_id attribute from database and attr_accessible and update the form views for ledger
-  has_many :page_types
+  has_many :page_types, dependent: :destroy
   has_many :pages, through: :page_types
 end
