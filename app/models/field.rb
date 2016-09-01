8356acf5661cc0808be3d46f0ee51b507d8bcdbc
Robert <robert@grenadine.co>
2016-09-01 10:13:40 -0400

    added data entries relationship to field

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,5 +2,7 @@ class Field < ActiveRecord::Base
   has_and_belongs_to_many :field_groups
   has_many :field_options, through: :field_options_fields
   has_many :field_options_fields, dependent: :destroy
+  has_many :data_entries
+  
   default_scope { order(position: :asc) }
 end
