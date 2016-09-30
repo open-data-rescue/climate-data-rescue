4069e0b75911346b8143eac6ada6eabb9f913863
Robert <robert@grenadine.co>
2016-09-30 13:34:55 -0400

    allow upload of all image types for content images

--- app/models/content_image.rb
+++ app/models/content_image.rb
@@ -2,18 +2,18 @@ class ContentImage < ActiveRecord::Base
 
     has_attached_file :image,
                   styles: { 
-                    thumb: ["64x64#", :jpg],
-                    small: ["200x200>", :jpg],
-                    medium: ["400x400>", :jpg],
-                    large: ["600x600>", :jpg],
-                    xlarge: ["1000x1000>", :jpg]
+                    thumb: ["64x64#"],
+                    small: ["200x200>"],
+                    medium: ["400x400>"],
+                    large: ["600x600>"],
+                    xlarge: ["1000x1000>"]
                   },
                   default_style: :medium,
                   url: "/system/:class/:style/:hash.:extension",
                   hash_secret: "SECRET"
     validates_attachment :image,
                         presence: true,
-                        content_type: { content_type: ["image/jpg","image/jpeg", "image/png"] }
+                        content_type: { :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)' }
 
     def name
         name = read_attribute(:name)
