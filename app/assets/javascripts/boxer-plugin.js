f9995fdab474290f10a9bc538bc5ef6754ff0b0d
Robert <robert@grenadine.co>
2016-10-13 22:54:23 -0400

    users can now draw boxes for new annotations
    Also fixed setting default positions for table guides

--- app/assets/javascripts/boxer-plugin.js
+++ app/assets/javascripts/boxer-plugin.js
@@ -4,71 +4,72 @@
 
 
 $.widget("ui.boxer", $.ui.mouse, {
-	_init: function() {
-		this.element.addClass("ui-boxer");
-		this.dragged = true;
-		this._mouseInit();
-		this.helper = $('<div></div>')
-			.addClass("annotation-marker helper");
+    _init: function() {
+        this.element.addClass("ui-boxer");
+        this.dragged = true;
+        this._mouseInit();
+        this.helper = $('<div></div>')
+            .addClass("annotation-marker helper");
       
-	},
+    },
 
-	destroy: function() {
+    destroy: function() {
        
-		this.element
-			.removeClass("ui-boxer ui-boxer-disabled")
-			.removeData("boxer")
-			.unbind(".boxer");
-		this._mouseDestroy();
+        this.element
+            .removeClass("ui-boxer ui-boxer-disabled")
+            .removeData("boxer")
+            .unbind(".boxer");
+        this._mouseDestroy();
 
-		return this;
-	},
+        return this;
+    },
 
-	_mouseStart: function(event) {
-		
+    _mouseStart: function(event) {
         var self = this;
         var options = this.options;
+        var target = options.target || options.appendTo;
         var groupColour = "";
         
         if (!(typeof(options.groupColour) == "undefined")) {
             groupColour = options.groupColour;
         };
 
-        var offset = $(options.appendTo).offset();
+        var offset = $(target).offset();
 
         var position = {
-	      x: event.pageX - $(document).scrollLeft() - offset.left,
-	      y : event.pageY - $(document).scrollTop() - offset.top
-	    };
+          x: event.pageX - $(document).scrollLeft() - offset.left,
+          y : event.pageY - $(document).scrollTop() - offset.top
+        };
 
-		this.opos = [position.x, position.y];
+        this.opos = [position.x, position.y];
 
-		if (this.options.disabled)
-			return;
+        if (this.options.disabled)
+            return;
 
-		
+        
 
-		this._trigger("start", event);
+        this._trigger("start", event);
 
-		$(options.appendTo).append(this.helper);
+        $(options.appendTo).append(this.helper);
 
-		this.helper.css({
-			"left": position.x,
-			"top": position.y,
-			"width": 0,
-			"height": 0
-		}).addClass(groupColour);
-	},
+        this.helper.css({
+            "left": position.x,
+            "top": position.y,
+            "width": 0,
+            "height": 0
+        }).addClass(groupColour);
+    },
 
-	_mouseDrag: function(event) {
+    _mouseDrag: function(event) {
       
         var self = this;
-		this.dragged = true;
+        this.dragged = true;
 
-		if (this.options.disabled)
-			return;
+        if (this.options.disabled)
+            return;
 
-		var options = this.options;
+        var options = this.options;
+        var target = options.target || options.appendTo;
         
         var $container = $(options.container);
         
@@ -85,50 +86,47 @@ $.widget("ui.boxer", $.ui.mouse, {
         };
 
         
-		var position = {
-	      x: event.pageX - $(document).scrollLeft() - $(options.appendTo).offset().left,
-	      y : event.pageY - $(document).scrollTop() - $(options.appendTo).offset().top
-	    };
+        var position = {
+          x: event.pageX - $(document).scrollLeft() - $(target).offset().left,
+          y : event.pageY - $(document).scrollTop() - $(target).offset().top
+        };
 
-		var x1 = this.opos[0], y1 = this.opos[1], x2 = position.x, y2 = position.y;
-		if (x1 > x2) { var tmp = x2; x2 = x1; x1 = tmp; }
-		this.helper.css({left: x1, top: y1, width: x2-x1, height: y2-y1});
-		
-		this._trigger("drag", event);
+        var x1 = this.opos[0], y1 = this.opos[1], x2 = position.x, y2 = position.y;
+        if (x1 > x2) { var tmp = x2; x2 = x1; x1 = tmp; }
+        if (y1 > y2) { var tmp = y2; y2 = y1; y1 = tmp; }
 
-		return false;
-	},
+        this.helper.css({left: x1, top: y1, width: x2-x1, height: y2-y1});
+        
+        this._trigger("drag", event);
 
-	_mouseStop: function(event) {
-//       alert('stop');
+        return false;
+    },
+
+    _mouseStop: function(event) {
           
-		var self = this;
+        var self = this;
 
-		this.dragged = false;
+        this.dragged = false;
 
-		var options = this.options;
+        var options = this.options;
 
-		var clone = this.helper.clone()
-      //.removeClass('ui-boxer-helper')
-			.appendTo(this.element).removeClass("helper");
+        var clone = this.helper.clone().appendTo(this.element).removeClass("helper");
      
 
-		this._trigger("stop", event, { box: clone });
+        this._trigger("stop", event, { box: clone });
 
-		    this.helper.remove(); 
-    //$( "div" ).remove( ".ui-boxer-helper" );
+        this.helper.remove(); 
     
-		return false;
+        return false;
       
-      
-	}
+    }
 
 });
 $.extend($.ui.boxer, { 
-	defaults: $.extend({}, $.ui.mouse.defaults, {
-		appendTo: 'body',
-		distance: 0
-	})
+    defaults: $.extend({}, $.ui.mouse.defaults, {
+        appendTo: 'body',
+        distance: 0
+    })
 });
 
 
