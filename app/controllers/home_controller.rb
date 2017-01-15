c0006c4a62cf02384b757faaa7145ded87b1533f
Robert <robert@grenadine.co>
2017-01-15 16:57:32 -0500

    SEO improvments and cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,5 @@ class HomeController < ApplicationController
   def index
     
   end
-  
-  def about
-    
-  end
-  
-  def science
-    
-  end
-  
-  def faq
-    
-  end
-  
-  def contact
-    
-  end
 
 end
--- app/helpers/pagetypes_helper.rb
+++
@@ -1,2 +0,0 @@
-module PagetypesHelper
-end
