2c860db078b0cadebe399b09cbe84206d87288bf
Robert Smith <robert@grenadine.co>
2017-02-09 11:24:16 -0500

    removed unused attributes from schema

--- db/schema.rb
+++ db/schema.rb
@@ -53,9 +53,7 @@ ActiveRecord::Schema.define(version: 20170130003321) do
     t.string   "help",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "page_type_id", limit: 4
     t.string   "display_name", limit: 255
-    t.integer  "position",     limit: 4,   default: 0,  null: false
     t.string   "colour_class", limit: 255, default: "", null: false
   end
 
@@ -95,24 +93,17 @@ ActiveRecord::Schema.define(version: 20170130003321) do
     t.string   "name",            limit: 255
     t.string   "field_key",       limit: 255
     t.string   "html_field_type", limit: 255
-    t.string   "initial_value",   limit: 255
-    t.text     "options",         limit: 65535
-    t.text     "validations",     limit: 65535
-    t.integer  "field_group_id",  limit: 4
     t.string   "data_type",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "full_name",       limit: 255
     t.text     "help",            limit: 65535
-    t.integer  "position",        limit: 4,     default: 0,     null: false
     t.boolean  "multi_select",                  default: false
   end
 
   create_table "ledgers", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.string   "ledger_type", limit: 255
-    t.date     "start_date"
-    t.date     "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -144,24 +135,21 @@ ActiveRecord::Schema.define(version: 20170130003321) do
   add_index "page_types_field_groups", ["page_type_id"], name: "index_page_types_field_groups_on_page_type_id", using: :btree
 
   create_table "pages", force: :cascade do |t|
-    t.string   "title",                limit: 255
-    t.integer  "height",               limit: 4
-    t.integer  "width",                limit: 4
-    t.integer  "order",                limit: 4
-    t.integer  "page_type_id",         limit: 4
-    t.boolean  "done",                             default: false, null: false
-    t.integer  "classification_count", limit: 4,   default: 0,     null: false
-    t.string   "accession_number",     limit: 255
+    t.string   "title",              limit: 255
+    t.integer  "height",             limit: 4
+    t.integer  "width",              limit: 4
+    t.integer  "page_type_id",       limit: 4
+    t.boolean  "done",                           default: false, null: false
+    t.string   "accession_number",   limit: 255
     t.date     "start_date"
     t.date     "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name",      limit: 255
-    t.string   "image_content_type",   limit: 255
-    t.integer  "image_file_size",      limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
-    t.integer  "transcriber_id",       limit: 4
-    t.string   "volume",               limit: 255
+    t.string   "volume",             limit: 255
   end
 
   add_index "pages", ["page_type_id"], name: "index_pages_on_page_type_id", using: :btree
@@ -221,7 +209,6 @@ ActiveRecord::Schema.define(version: 20170130003321) do
     t.string   "display_name",           limit: 255
     t.boolean  "admin"
     t.text     "bio",                    limit: 65535
-    t.integer  "num_contributions",      limit: 4,     default: 0,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar_file_name",       limit: 255
