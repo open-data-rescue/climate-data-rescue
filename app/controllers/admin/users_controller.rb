63d5319aa284d3b40823af9fa61968906a2dcb82
Robert Smith <robert@grenadine.co>
2017-03-04 12:07:18 -0500

    add ability to edit user from admin page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,20 +8,45 @@ module Admin
     def show
       @user = User.find params[:id]
     end
-    
-    def destroy
-      User.transaction do
-          begin
-            @user = User.find(params[:id])
-            @user.destroy
-          rescue => e
-            flash[:danger] = e.message
-          end
+
+    def edit
+      @user = User.find params[:id]
+    end
+
+    def update
+      begin
+        User.transaction do
+          @user = User.find params[:id]
+          @user.update_attributes(users_params)
+        end
+      rescue => e
+        flash[:danger] = e.message
       end
 
       respond_to do |format|
         format.html { redirect_to admin_users_path }
       end
+    end
+    
+    def destroy
+      begin
+        User.transaction do
+          @user = User.find(params[:id])
+          @user.destroy
+        end
+      rescue => e
+        flash[:danger] = e.message
+      end
+
+      respond_to do |format|
+        format.html { redirect_to admin_users_path }
+      end
+    end
+
+    private
+
+    def users_params
+      params.require(:user).permit(:display_name, :admin, :bio)
     end
 
   end
