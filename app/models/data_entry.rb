1ed24e036583872deb1dd7347e5a7908d70f175b
Unknown <robert@grenadine.co>
2017-10-07 16:13:07 -0400

    add validators to data entry model

--- app/models/data_entry.rb
+++ app/models/data_entry.rb
@@ -4,4 +4,8 @@ class DataEntry < ActiveRecord::Base
 	belongs_to :annotation, touch: true
 	belongs_to :field
 
+  validates :field_id, uniqueness: { scope: :annotation_id }, presence: true
+  validates :annotation_id, presence: true
+  validates :page_id, presence: true
+
 end
