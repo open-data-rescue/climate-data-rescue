f22d078ef9135bf108ee7b85d964b821b77f1c58
Unknown <robert@grenadine.co>
2017-10-07 21:56:26 -0400

    add validations to transcription model

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -2,11 +2,14 @@ class Transcription < ActiveRecord::Base
   #attributes that can be called on the model object
   #attr_accessible :page_data
   belongs_to :page
+  belongs_to :user
   has_one :page_type, through: :page
   has_many :field_groups, through: :page_type
   has_many :fields, through: :field_groups
-  belongs_to :user
   has_many :annotations, dependent: :destroy
+
+  validates :page_id, uniqueness: { scope: :user_id }, presence: true
+  validates :user_id, presence: true
   
 
   def unique_observation_dates
