7aa0a452c04eb06d32a2ee4a307760e3d7d22b56
Robert Smith <robert@grenadine.co>
2018-08-19 18:31:12 -0400

    added spec coverage for ledger model (8.9% total)

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,6 +1,9 @@
 class Ledger < ActiveRecord::Base
-  #attr_accessible :author, :extern_ref, :title, :pagetype_id
-  #TODO: remove :pagetype_id attribute from database and attr_accessible and update the form views for ledger
   has_many :page_types, dependent: :destroy
   has_many :pages, through: :page_types
+
+  validates :title,
+            presence: true
+  validates :ledger_type,
+            presence: true
 end
