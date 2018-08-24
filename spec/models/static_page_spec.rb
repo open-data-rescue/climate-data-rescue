c76a00edc89d951ba73eae0fd88531d896791332
Robert Smith <robert@grenadine.co>
2018-08-24 08:28:10 -0400

    added more specs for StaticPage

--- spec/models/static_page_spec.rb
+++ spec/models/static_page_spec.rb
@@ -10,6 +10,8 @@ describe StaticPage do
   end
 
   describe 'ActiveRecord associations' do
+    it { is_expected.to have_many(:children) }
+    it { is_expected.to belong_to(:parent) }
   end
 
   describe 'ActiveModel validations' do
@@ -21,6 +23,24 @@ describe StaticPage do
 
   describe 'callbacks' do
     it { is_expected.to callback(:update_positions_and_slug).before(:save) }
+  end
+
+  describe 'scopes' do
+    describe '.visible' do
+      it { expect(klass).to respond_to(:visible).with(1).argument }
+    end
+    describe '.top_level' do
+      it { expect(klass).to respond_to(:top_level).with(1).argument }
+    end
+    describe '.header_links' do
+      it { expect(klass).to respond_to(:header_links).with(1).argument }
+    end
+    describe '.footer_links' do
+      it { expect(klass).to respond_to(:footer_links).with(1).argument }
+    end
+    describe '.transcriber_links' do
+      it { expect(klass).to respond_to(:transcriber_links).with(1).argument }
+    end
   end
 
   describe '#is_external?' do
@@ -51,5 +71,9 @@ describe StaticPage do
         expect(static_page.link).to eq("/#{I18n.locale}#{static_page.slug}")
       end
     end
+  end
+
+  describe '.matches?' do
+    it { expect(klass).to respond_to(:matches?).with(1).argument }
   end
 end
