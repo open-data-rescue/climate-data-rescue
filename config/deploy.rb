f43c2a654c02ea8510e7fb7d27440207d1bbec33
Robert Smith <robert@grenadine.co>
2018-09-09 14:11:45 -0400

    stop checking branch head for now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,11 +35,11 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
+      # unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      #   puts "WARNING: HEAD is not the same as origin/master"
+      #   puts "Run `git push` to sync changes."
+      #   exit
+      # end
     end
   end
 
