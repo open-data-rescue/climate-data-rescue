dbb66f8becc16b25c4f5b63693e47bb1cedd065c
Robert Smith <rsmithlal@gmail.com>
2019-09-17 12:00:34 -0400

    fix staging and production log level

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,7 @@
 DataRescueAtHome::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  config.log_level = :debug
+  config.log_level = :error
   
   config.eager_load = true
   
