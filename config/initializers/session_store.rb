de0459600a88f19358e0676b3eeb9bc15c650b59
Robert Smith <rsmithlal@gmail.com>
2019-09-24 14:21:58 -0400

    fix line length

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-# DataRescueAtHome::Application.config.session_store :cookie_store, key: '_draw_session'
+# DataRescueAtHome::Application.config.session_store :cookie_store,
+#                                                    key: '_draw_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-DataRescueAtHome::Application.config.session_store :active_record_store, key: '_draw_session'
+DataRescueAtHome::Application.config.session_store :active_record_store,
+                                                   key: '_draw_session'
