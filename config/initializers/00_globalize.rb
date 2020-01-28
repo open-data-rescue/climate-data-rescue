1efd0138957489ec307bac6f74d805608fb81935
Robert Smith <rsmithlal@gmail.com>
2020-01-28 09:33:07 -0500

    reconfigure database yml. Disable globalize path that allowed docker production build

--- config/initializers/00_globalize.rb
+++ config/initializers/00_globalize.rb
@@ -1,19 +1,19 @@
 
-module Globalize
-  module ActiveRecord
-    module ActMacro
-      alias_method :old_check_columns!, :check_columns!
-      def check_columns!(attr_names)
-        return if ::ActiveRecord::VERSION::STRING >= "5.0" || ! connected?
-        old_check_columns!(attr_names)
-      end
+# module Globalize
+#   module ActiveRecord
+#     module ActMacro
+#       alias_method :old_check_columns!, :check_columns!
+#       def check_columns!(attr_names)
+#         return if ::ActiveRecord::VERSION::STRING >= "5.0" || ! connected?
+#         old_check_columns!(attr_names)
+#       end
 
 
-      alias_method :old_allow_translation_of_attributes, :allow_translation_of_attributes
-      def allow_translation_of_attributes(attr_names)
-        return if ::ActiveRecord::VERSION::STRING >= "5.0" || ! connected?
-        old_allow_translation_of_attributes!(attr_names)
-      end
-    end
-  end
-end
+#       alias_method :old_allow_translation_of_attributes, :allow_translation_of_attributes
+#       def allow_translation_of_attributes(attr_names)
+#         return if ::ActiveRecord::VERSION::STRING >= "5.0" || ! connected?
+#         old_allow_translation_of_attributes!(attr_names)
+#       end
+#     end
+#   end
+# end
