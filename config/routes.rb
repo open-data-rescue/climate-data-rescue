6588909ff3a2862b262daff7558b799cad6df72d
Robert Smith <rsmithlal@gmail.com>
2020-12-31 01:52:06 -0330

    ensure that API endpoints require login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :pages, except: %i[new edit]
+  authenticate :user do
+    namespace :api do
+      namespace :v1 do
+        resources :pages, except: %i[new edit]
+      end
     end
   end
 
