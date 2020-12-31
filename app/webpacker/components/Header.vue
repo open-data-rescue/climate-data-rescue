fde4f1322da3b0261d57821df775703e4512b884
Robert Smith <rsmithlal@gmail.com>
2020-12-31 01:51:18 -0330

    Added localized logo to navbar. Added full admin nav item list, my profile and logout links. Styled new navbar to match original.

--- app/webpacker/components/Header.vue
+++ app/webpacker/components/Header.vue
@@ -1,12 +1,20 @@
 
 <template>
   <b-navbar
+    id="header-nav"
+    class="py-0"
     toggleable="lg"
     type="dark"
     variant="dark"
   >
     <b-navbar-brand href="/">
-      DRAW
+      <b-img
+        id="header-draw-logo"
+        alt="DRAW"
+        fluid
+        :src="localizedLogo"
+      />
+      Manager
     </b-navbar-brand>
 
     <b-navbar-nav class="ml-auto">
@@ -29,23 +37,41 @@
       </b-nav-item-dropdown>
 
       <b-nav-item-dropdown
-        text="Better Together"
-        right
-      >
-        <b-dropdown-item href="#">
-          About Better Together
-        </b-dropdown-item>
-      </b-nav-item-dropdown>
-
-      <b-nav-item-dropdown
         text="Admin"
         right
       >
         <b-dropdown-item :to="{ name: 'pages', params: { locale: locale } }">
           Pages
         </b-dropdown-item>
-       <b-dropdown-item :to="{ name: 'transcriptions', params: { locale: locale }}">
+        <b-dropdown-item :href="localizedPath('/admin/transcriptions')">
           Transcriptions
+        </b-dropdown-item>
+        <b-dropdown-divider />
+
+        <b-dropdown-item :href="localizedPath('/admin/page_types')">
+          Schemas
+        </b-dropdown-item>
+        <b-dropdown-item :href="localizedPath('/admin/field_groups')">
+          Field Groups
+        </b-dropdown-item>
+        <b-dropdown-item :href="localizedPath('/admin/fields')">
+          Fields
+        </b-dropdown-item>
+        <b-dropdown-item :href="localizedPath('/admin/field_options')">
+          Field Values
+        </b-dropdown-item>
+        <b-dropdown-divider />
+
+        <b-dropdown-item :href="localizedPath('/admin/static_pages')">
+          Custom Pages
+        </b-dropdown-item>
+        <b-dropdown-item :href="localizedPath('/admin/content_images')">
+          Content Images
+        </b-dropdown-item>
+        <b-dropdown-divider />
+
+        <b-dropdown-item :href="localizedPath('/admin/users')">
+          Users
         </b-dropdown-item>
       </b-nav-item-dropdown>
 
@@ -54,10 +80,13 @@
         <template #button-content>
           <em>User</em>
         </template>
-        <b-dropdown-item href="#">
+        <b-dropdown-item :href="localizedPath('/my-profile')">
           Profile
         </b-dropdown-item>
-        <b-dropdown-item href="#">
+        <b-dropdown-item
+          href="/logout"
+          data-method="DELETE"
+        >
           Sign Out
         </b-dropdown-item>
       </b-nav-item-dropdown>
@@ -66,21 +95,66 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import DrawLogoEn from '../../assets/images/drawlogo_en.png'
+import DrawLogoFr from '../../assets/images/drawlogo_fr.png'
+import UrlHelpers from '../mixins/UrlHelpers'
 
 export default {
   name: 'Header',
   computed: {
-    ...mapState(['locale']),
     currentRouteName () {
       return this.$route.name
+    },
+    localizedLogo () {
+      return this.locale === 'en' ? DrawLogoEn : DrawLogoFr
     }
-  }
+  },
+  mixins: [UrlHelpers]
 }
 </script>
 
-<style lang="scss">
-  .bg-dark {
-    background: rgba(0, 0, 0, 0.8) !important;
+<style scoped lang="scss">
+  @import 'stylesheets/draw-variables';
+
+  #admin-app {
+    #header-draw-logo {
+      height: 100%;
+      display: inline-block;
+    }
+
+    .bg-dark {
+      background: rgba(0, 0, 0, 0.8) !important;
+    }
+
+    #header-nav {
+      /deep/ a {
+        color: #ffffff;
+
+        &:not(.navbar-brand):hover {
+          background-color: $accent-color;
+        }
+
+        &.dropdown-item, {
+          color: #2c3e50;
+
+          &:hover {
+            color: #ffffff;
+          }
+        }
+      }
+
+      /deep/ .dropdown-menu {
+        background: rgba(0, 0, 0, 0.8);
+
+        a {
+          color: #ffffff;
+
+          &:hover {
+            background-color: $accent-color;
+          }
+        }
+      }
+    }
   }
+
 </style>
