cbe73532dca0ac22b022ec7214534d0729ee775e
Robert Smith <rsmithlal@gmail.com>
2020-12-31 18:22:39 -0330

    NOW() not supported for server MySQL

--- db/migrate/20201231200247_add_timestamps_to_page_days.rb
+++ db/migrate/20201231200247_add_timestamps_to_page_days.rb
@@ -2,7 +2,7 @@ class AddTimestampsToPageDays < ActiveRecord::Migration[5.2]
   def change
     change_table :page_days do |t|
       t.integer :lock_version, null: false, default: 0
-      t.timestamps null: false, default: -> { 'NOW()' }
+      t.timestamps null: false, default: -> { DateTime.now }
     end
   end
 end
