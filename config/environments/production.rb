8d310e890412ce4ff9c3c381e70a598330498c90
Robert Smith <rsmithlal@gmail.com>
2021-07-06 11:27:35 -0230

    set debug logging when stdout enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.log_level = :debug
   end
 
   # Do not dump schema after migrations.
