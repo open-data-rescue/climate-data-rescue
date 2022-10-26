c7f310d0c5eda8a44cb5b1fb3e8915c982b1624c
balen <henry.balen@gmail.com>
2022-10-26 12:11:37 -0400

    Migrate to R6

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.6.7'
+ruby '2.7.3'
 
 gem 'rails', '~> 6.1'
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,51 @@
+// const { environment } = require('@rails/webpacker')
+// const isProduction = process.env.NODE_ENV === 'production'
+
+// const vue = require('./loaders/vue')
+// environment.loaders.append('vue', vue)
+
+// if (!isProduction) {
+//   const eslint = require('./loaders/eslint')
+//   environment.loaders.append('eslint', eslint)
+// }
+
+// module.exports = environment
+
+
 const { environment } = require('@rails/webpacker')
-const isProduction = process.env.NODE_ENV === 'production'
-
+const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-environment.loaders.append('vue', vue)
+const erb = require('./loaders/erb')
 
-if (!isProduction) {
-  const eslint = require('./loaders/eslint')
-  environment.loaders.append('eslint', eslint)
+const webpack = require('webpack')
+// const isProduction = process.env.NODE_ENV === 'production'
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('erb', erb)
+
+// if (!isProduction) {
+//   const eslint = require('./loaders/eslint')
+//   environment.loaders.append('eslint', eslint)
+// }
+
+var path = require('path');
+
+environment.plugins.prepend('env',
+  new webpack.DefinePlugin({
+    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+)
+
+// added and allow BootstrapVue to work.
+const config = environment.toWebpackConfig()
+config.resolve = {
+  alias: {
+    'vue$': 'vue/dist/vue.esm.js',
+    '@': path.resolve(__dirname, '../../app/webpacker'),
+    '@mixins': '@/mixins'
+  },
+  extensions: ['*', '.js', '.vue', '.json']
 }
 
 module.exports = environment
