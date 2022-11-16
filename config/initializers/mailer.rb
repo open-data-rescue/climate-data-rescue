c1b32ef29e9a2fe57d686a81d33fc02cbafd24e6
balen <henry.balen@gmail.com>
2022-11-16 16:46:09 -0500

    adjust smtp init

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -28,15 +28,21 @@ Rails.application.config.action_mailer.tap do |action_mailer|
       action_mailer.perform_deliveries = true
       action_mailer.raise_delivery_errors = true
       action_mailer.smtp_settings = {
-        address: ENV.fetch('SMTP_SERVER', 'smtp.gmail.com'),
+        address: ENV.fetch('SMTP_SERVER', 'localhost'),
         port: ENV.fetch("SMTP_PORT") { 25 },
         domain: ENV.fetch('SMTP_DOMAIN', nil),
-        user_name: ENV.fetch('SMTP_USERNAME', nil),
-        password: ENV.fetch('SMTP_PASSWORD', nil),
-        authentication: ENV.fetch('SMTP_AUTHENTICATION', nil), # plain etc
-        enable_starttls_auto: ENV.fetch('SMTP_SSL_START_AUTO', nil),
-        openssl_verify_mode: ENV.fetch('SMTP_SSL_VERIFY_MODE', nil)
       }
+
+      user_name = ENV.fetch('SMTP_USERNAME', nil)
+      action_mailer.smtp_settings[:user_name] if user_name
+      password = ENV.fetch('SMTP_PASSWORD', nil)
+      action_mailer.smtp_settings[:password] if password
+      authentication = ENV.fetch('SMTP_AUTHENTICATION', nil) # plain etc
+      action_mailer.smtp_settings[:authentication] if authentication
+      enable_starttls_auto = ENV.fetch('SMTP_SSL_START_AUTO', nil)
+      action_mailer.smtp_settings[:enable_starttls_auto] if enable_starttls_auto
+      openssl_verify_mode = ENV.fetch('SMTP_SSL_VERIFY_MODE', nil)
+      action_mailer.smtp_settings[:openssl_verify_mode] if openssl_verify_mode
     end
   end
 end
