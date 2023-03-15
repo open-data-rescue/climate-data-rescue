99db2d7299854b982e28aeef33b8deea574dcb79
David Maciver <david.maciver@greenspacelive.com>
2023-03-15 12:56:55 +0000

    Renamed DataEntriesAuditDetails to DataEntriesAuditDetail.

--- app/models/views/data_entries_audit_detail.rb
+++ app/models/views/data_entries_audit_detail.rb
@@ -1,4 +1,4 @@
-class Views::DataEntriesAuditDetails < ApplicationRecord
+class Views::DataEntriesAuditDetail < ApplicationRecord
   self.table_name = :date_entries_audit_details
   # self.primary_key = :id
 end
