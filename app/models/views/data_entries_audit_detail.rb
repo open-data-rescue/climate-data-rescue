a4bb4a52211e31d8daad27616cd50ed12c184881
David Maciver <david.maciver@greenspacelive.com>
2023-03-15 12:58:54 +0000

    Fixed naming of DataEntriesAuditDetail table_name.

--- app/models/views/data_entries_audit_detail.rb
+++ app/models/views/data_entries_audit_detail.rb
@@ -1,4 +1,4 @@
 class Views::DataEntriesAuditDetail < ApplicationRecord
-  self.table_name = :date_entries_audit_details
+  self.table_name = :data_entries_audit_details
   # self.primary_key = :id
 end
